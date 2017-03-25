471530db6b86dee78b85001a10fe34299e4b3434
Jon Friis <friis.j@gmail.com>
2017-03-25 10:49:44 -0400

    Legacy layout adjustments

--- _site/js/layout.js
+++ _site/js/layout.js
@@ -1,18 +1,15 @@
+$('#topbar').scrollupbar();
+
 $(document).ready(function(){
-    $('.show--contact').click(function(){
-        console.log('show contact');
-        $('body').addClass('visible--contact');
-        $('.sg-menu').removeClass('visible');
-        $('.sg-logo').addClass('invert');
-        $('.toggle').addClass('visible').addClass('invert').addClass('toggle--close');
+
+    $('.toggle--menu').click(function(){
+        $(this).toggleClass('toggle--close');
+        $('.sg-menu').toggleClass('visible--mobile');
     });
 
-    $('.toggle--close').click(function(){
-        console.log('close contact');
-        $('body').removeClass('visible--contact');
-        $('.sg-menu').addClass('visible');
-        $('.sg-logo').removeClass('invert');
-        $(this).removeClass('visible').removeClass('toggle--close');
+    $('.toggle--contact').click(function(){
+        $('.toggle--menu').removeClass('toggle--close');
+        $('.sg-menu').removeClass('visible--mobile');
     });
 
     $('.block--gallery').slick({
@@ -22,9 +19,4 @@ $(document).ready(function(){
 		fade: true,
 		cssEase: 'linear'
 	});
-
-    $('.toggle--overlay').click(function(){
-        $(this).toggleClass('close');
-        $('.sg-menu').toggleClass('visible--mobile');
-    });
 });
